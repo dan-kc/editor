ce2c23267678fd6af06e21df0227c1ecdfac5b46
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2024-03-23 11:47:00 +0000

    rename

--- src/buffer.rs
+++ src/buffer.rs
@@ -7,14 +7,14 @@ use ropey::Rope;
 
 #[derive(Debug, Default)]
 pub struct Buffer {
-    pub file: String,
+    pub file_name: String,
     pub text: Rope,
 }
 
 impl Buffer {
     pub fn from_file(file: String) -> Result<Buffer, io::Error> {
         let text = Rope::from_reader(BufReader::new(File::open(file.clone())?))?;
-        let buf = Buffer { file, text };
+        let buf = Buffer { file_name: file, text };
         Ok(buf)
     }
 }
